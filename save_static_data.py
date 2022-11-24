b84613790f62c92e43d7ffab94f47eaa1908417a
Andy Gnias <AGnias47@gmail.com>
2022-11-24 10:57:20 -0500

    Save data

--- save_static_data.py
+++ save_static_data.py
@@ -5,3 +5,5 @@ from team.dataframe import get_all_d1_schools, add_ap_rankings_to_dataframe
 df = get_all_d1_schools()
 add_ap_rankings_to_dataframe(df)
 df.to_pickle("saved_static_data/school_data_dataframe.pkl")
+df.to_json("saved_static_data/school_data_dataframe.json")
+
