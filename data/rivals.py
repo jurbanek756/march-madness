03b3e1847cec58b9d437cf23d48c5c6afbf06dca
Andy Gnias <AGnias47@gmail.com>
2022-11-25 13:22:14 -0500

    Rivalries added

--- data/rivals.py
+++ data/rivals.py
@@ -1,3 +1,8 @@
+"""
+Sources:
+- https://bleacherreport.com/articles/1719358-ranking-the-25-best-rivalries-in-college-basketball
+"""
+
 RIVALRIES = {
     "Duke University": "University of North Carolina",
     "University of Michiagan": "Ohio State University",
@@ -9,5 +14,20 @@ RIVALRIES = {
     "University of Michigan": "Michigan State University",
     "University of Pennsylvania": "Princeton University",
     "Harvard University": "Yale University",
+    "University of Arkansas": "University of Missouri",
+    "Syracuse University": "University of Pittsburgh",
+    "Marquette University": "University of Notre Dame",
+    "University of Iowa": "Iowa State University",
+    "Stanford University": "University of California-Berkeley",
+    "University of Illinois-Urbana-Champagin": "University of Missouri",
+    "Marquette University": "University of Wisconsin",
+    "Villanova University": "LaSalle University",
+    "University of Connecticut": "Syracuse University",
+    "Indiana University": "University of Kentucky",
+    "Duke University": "University of Maryland",
+    "Syracuse University": "Georgetown University",
+    "University of Kentucky": "University of Florida",
+    "Georgetown University": "Villanova University",
+    "University of California-Los Angeles": "Unversity of Arizona"
 }
 RIVALRIES = RIVALRIES | {v: k for k, v in RIVALRIES.items()}
