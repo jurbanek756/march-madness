87197339f3e1150900bd683c095855308c4c5314
Andy Gnias <AGnias47@gmail.com>
2022-11-25 22:42:52 -0500

    Sample run added

--- run.py
+++ run.py
@@ -15,24 +15,39 @@ df = filter_none_values(df, "Tournament Ranking")
 
 teams = [Team(record) for record in df.to_dict(orient="records")]
 
-north = [[teams[0], teams[1]]]
-south = [[teams[2], teams[3]]]
-east = [[teams[4], teams[5]]]
-west = [[teams[6], teams[7]]]
+# Format is <conference>_<expected seeds in each game>, ex. east_116 will be the first and 16th seed playing in the
+#   east conference, west_81 is the game in the western conference where the 8 and 1 seed are most likely to play
+#   but could actually be the 9 and 1 seed
+north_12 = [[teams[0], teams[1]]]
+south_12 = [[teams[2], teams[3]]]
+east_12 = [[teams[4], teams[5]]]
+west_12 = [[teams[6], teams[7]]]
 
 
-print(f"{north[0][0]} vs {north[0][1]} prediction:", end=" ")
-north = weighted_random_selection(north[0][0], north[0][1])
+print(f"{north_12[0][0]} vs {north_12[0][1]} prediction:", end=" ")
+north = weighted_random_selection(north_12[0][0], north_12[0][1])
 print(north)
 
-print(f"{south[0][0]} vs {south[0][1]} prediction:", end=" ")
-south = weighted_random_selection(south[0][0], south[0][1])
+print(f"{south_12[0][0]} vs {south_12[0][1]} prediction:", end=" ")
+south = weighted_random_selection(south_12[0][0], south_12[0][1])
 print(south)
 
-print(f"{east[0][0]} vs {east[0][1]} prediction:", end=" ")
-east = weighted_random_selection(east[0][0], east[0][1])
+print(f"{east_12[0][0]} vs {east_12[0][1]} prediction:", end=" ")
+east = weighted_random_selection(east_12[0][0], east_12[0][1])
 print(east)
 
-print(f"{west[0][0]} vs {west[0][1]} prediction:", end=" ")
-west = weighted_random_selection(west[0][0], west[0][1])
+print(f"{west_12[0][0]} vs {west_12[0][1]} prediction:", end=" ")
+west = weighted_random_selection(west_12[0][0], west_12[0][1])
 print(west)
+
+print(f"{north} vs {south} prediction:", end=" ")
+ns = weighted_random_selection(north, south)
+print(ns)
+
+print(f"{east} vs {west} prediction:", end=" ")
+ew = weighted_random_selection(east, west)
+print(ew)
+
+print(f"Championship Game between {ns} and {ew}")
+champion = weighted_random_selection(ns, ew)
+print(f"Champion: {champion}")
