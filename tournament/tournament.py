4e1c6c937f65fdb4fba125b9b5610030a305afe9
Andy Gnias <AGnias47@gmail.com>
2022-11-26 10:19:02 -0500

    Async run type

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import logging
 from predict.select_team import weighted_random_selection
 
 
@@ -10,22 +13,38 @@ class Tournament:
         midwest,
         play_in_rank,
         prediction_method=weighted_random_selection,
+        log_results=True
     ):
+        if log_results:
+            logging.basicConfig(
+                encoding="UTF-8",
+                level="INFO",
+                handlers=[logging.FileHandler("NCAA_Tournament_Results.txt"), logging.StreamHandler()],
+                format='%(message)s'
+            )
         self.play_in_rank = play_in_rank
         self.predict = prediction_method
-        self.west = Group(west, self.predict, self.play_in_rank)
-        self.south = Group(south, self.predict, self.play_in_rank)
-        self.east = Group(east, self.predict, self.play_in_rank)
-        self.midwest = Group(midwest, self.predict, self.play_in_rank)
+        self.west = Group(west, self.predict, self.play_in_rank, log_results)
+        self.south = Group(south, self.predict, self.play_in_rank, log_results)
+        self.east = Group(east, self.predict, self.play_in_rank, log_results)
+        self.midwest = Group(midwest, self.predict, self.play_in_rank, log_results)
+        self.west_winner = None
+        self.east_winner = None
+        self.south_winner = None
+        self.midwest_winner = None
 
     def tournament_winner(self):
-        left = self.predict(self.west, self.east)
-        right = self.predict(self.south, self.midwest)
+        self.west_winner = self.west.run()
+        self.east_winner = self.east.run()
+        self.south_winner = self.south.run()
+        self.midwest_winner = self.midwest.run()
+        left = self.predict(self.west_winner, self.east_winner)
+        right = self.predict(self.south_winner, self.midwest_winner)
         return self.predict(left, right)
 
 
 class Group:
-    def __init__(self, ranking_dict, prediction_method, play_in_rank):
+    def __init__(self, ranking_dict, prediction_method, play_in_rank, log_results=True):
         self.one = ranking_dict["one"]
         self.two = ranking_dict["two"]
         self.three = ranking_dict["three"]
@@ -45,17 +64,15 @@ class Group:
         self.play_in = ranking_dict["play_in"]
         self.predict = prediction_method
         self.play_in_rank = play_in_rank
-        self.group_winner = self.run()
+        self.log_results = log_results
+        if self.log_results:
+            logging.info("Group Rankings:")
+            logging.info(self.ranking_dict)
+        self.group_winner = None
 
-    def run(self):
-        self.first_four()
-        first_round_results = self.first_round()
-        second_round_results = self.second_round(first_round_results)
-        sweet_sixteen_results = self.sweet_sixteen(second_round_results)
-        return self.elite_eight(sweet_sixteen_results)
-
-    def first_four(self):
-        ranking_dict = {
+    @property
+    def ranking_dict(self):
+        return{
             1: self.one,
             2: self.two,
             3: self.three,
@@ -72,13 +89,28 @@ class Group:
             14: self.fourteen,
             15: self.fifteen,
             16: self.sixteen,
+            "Play In": self.play_in
         }
-        ranking_dict[self.play_in_rank] = self.predict(
-            self.play_in, ranking_dict[self.play_in_rank]
+
+    def run(self):
+        self.first_four()
+        first_round_results = self.first_round()
+        second_round_results = self.second_round(first_round_results)
+        sweet_sixteen_results = self.sweet_sixteen(second_round_results)
+        self.group_winner = self.elite_eight(sweet_sixteen_results)
+        if self.log_results:
+            logging.info(f"Group Winner: {self.group_winner}")
+        return self.group_winner
+
+    def first_four(self):
+        self.ranking_dict[self.play_in_rank] = self.predict(
+            self.play_in, self.ranking_dict[self.play_in_rank]
         )
+        if self.log_results:
+            logging.info("First Four Winner: %s", self.ranking_dict[self.play_in_rank])
 
     def first_round(self):
-        return {
+        first_round_results = {
             "1_16": self.predict(self.one, self.sixteen),
             "2_15": self.predict(self.two, self.fifteen),
             "3_14": self.predict(self.three, self.fourteen),
@@ -88,20 +120,32 @@ class Group:
             "7_10": self.predict(self.seven, self.ten),
             "8_9": self.predict(self.eight, self.nine),
         }
+        if self.log_results:
+            logging.info("First Round Results:")
+            logging.info(first_round_results)
+        return first_round_results
 
     def second_round(self, first_round_results):
-        return {
+        second_round_results = {
             "1_8": self.predict(first_round_results["1_16"], first_round_results["8_9"]),
             "2_7": self.predict(first_round_results["2_15"], first_round_results["7_10"]),
             "3_6": self.predict(first_round_results["3_14"], first_round_results["6_11"]),
             "4_5": self.predict(first_round_results["4_13"], first_round_results["5_12"]),
         }
+        if self.log_results:
+            logging.info("Second Round Results:")
+            logging.info(second_round_results)
+        return second_round_results
 
     def sweet_sixteen(self, second_round_results):
-        return {
+        sweet_sixteen_results = {
             "1_4": self.predict(second_round_results["1_8"], second_round_results["4_5"]),
             "2_3": self.predict(second_round_results["2_7"], second_round_results["3_6"]),
         }
+        if self.log_results:
+            logging.info("Sweet Sixteen Results")
+            logging.info(sweet_sixteen_results)
+        return sweet_sixteen_results
 
     def elite_eight(self, sweet_sixteen_results):
         return self.predict(sweet_sixteen_results["1_4"], sweet_sixteen_results["2_3"])
