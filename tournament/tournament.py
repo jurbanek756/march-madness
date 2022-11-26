9712effb2ba0a7ba870a69d77e6b096c5d88ca2f
Andy Gnias <AGnias47@gmail.com>
2022-11-26 10:42:51 -0500

    Tournament class complete; untested

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -13,14 +13,18 @@ class Tournament:
         midwest,
         play_in_rank,
         prediction_method=weighted_random_selection,
-        log_results=True
+        log_results=True,
     ):
-        if log_results:
+        self.log_results = log_results
+        if self.log_results:
             logging.basicConfig(
                 encoding="UTF-8",
                 level="INFO",
-                handlers=[logging.FileHandler("NCAA_Tournament_Results.txt"), logging.StreamHandler()],
-                format='%(message)s'
+                handlers=[
+                    logging.FileHandler("NCAA_Tournament_Results.txt"),
+                    logging.StreamHandler(),
+                ],
+                format="%(message)s",
             )
         self.play_in_rank = play_in_rank
         self.predict = prediction_method
@@ -33,14 +37,190 @@ class Tournament:
         self.south_winner = None
         self.midwest_winner = None
 
-    def tournament_winner(self):
-        self.west_winner = self.west.run()
-        self.east_winner = self.east.run()
-        self.south_winner = self.south.run()
-        self.midwest_winner = self.midwest.run()
-        left = self.predict(self.west_winner, self.east_winner)
-        right = self.predict(self.south_winner, self.midwest_winner)
+    def run(self):
+        west_winner, east_winner, south_winner, midwest_winner = self.group_winners()
+        tournament_winner = self.tournament_winner(
+            west_winner, east_winner, south_winner, midwest_winner
+        )
+        if self.log_results:
+            logging.info("NCAA Champions: %s", tournament_winner)
+        return tournament_winner
+
+    def group_winners(self):
+        self.first_four()
+        (
+            west_first_round,
+            east_first_round,
+            south_first_round,
+            midwest_first_round,
+        ) = self.first_round()
+        (
+            west_second_round,
+            east_second_round,
+            south_second_round,
+            midwest_second_round,
+        ) = self.second_round(
+            west_first_round, east_first_round, south_first_round, midwest_first_round
+        )
+
+        (
+            west_sweet_sixteen,
+            east_sweet_sixteen,
+            south_sweet_sixteen,
+            midwest_sweet_sixteen,
+        ) = self.sweet_sixteen(
+            west_first_round, east_first_round, south_first_round, midwest_first_round
+        )
+
+        return self.elite_eight(
+            west_sweet_sixteen,
+            east_sweet_sixteen,
+            south_sweet_sixteen,
+            midwest_sweet_sixteen,
+        )
+
+    def tournament_winner(self, west_winner, east_winner, south_winner, midwest_winner):
+        left = self.predict(west_winner, east_winner)
+        right = self.predict(south_winner, midwest_winner)
+        if self.log_results:
+            logging.info("Championship Game: %s vs. %s", left, right)
         return self.predict(left, right)
+
+    def first_four(self):
+        west_first_four = self.west.first_four()
+        if self.log_results:
+            logging.info("West First Four Winner: %s", west_first_four)
+
+        east_first_four = self.east.first_four()
+        if self.log_results:
+            logging.info("East First Four Winner: %s", east_first_four)
+
+        south_first_four = self.south.first_four()
+        if self.log_results:
+            logging.info("South First Four Winner: %s", south_first_four)
+
+        midwest_first_four = self.midwest.first_four()
+        if self.log_results:
+            logging.info("Midwest First Four Winner: %s", midwest_first_four)
+
+        return west_first_four, east_first_four, south_first_four, midwest_first_four
+
+    def first_round(self):
+        west_first_round = self.west.first_round()
+        if self.log_results:
+            logging.info("West First Round Results:")
+            logging.info(west_first_round)
+
+        east_first_round = self.east.first_round()
+        if self.log_results:
+            logging.info("East First Round Results:")
+            logging.info(east_first_round)
+
+        south_first_round = self.south.first_round()
+        if self.log_results:
+            logging.info("South First Round Results:")
+            logging.info(south_first_round)
+
+        midwest_first_round = self.midwest.first_round()
+        if self.log_results:
+            logging.info("Midwest First Round Results:")
+            logging.info(midwest_first_round)
+
+        return (
+            west_first_round,
+            east_first_round,
+            south_first_round,
+            midwest_first_round,
+        )
+
+    def second_round(
+        self, west_first_round, east_first_round, south_first_round, midwest_first_round
+    ):
+        west_second_round = self.west.second_round(west_first_round)
+        if self.log_results:
+            logging.info("West Second Round Results:")
+            logging.info(west_second_round)
+
+        east_second_round = self.east.second_round(east_first_round)
+        if self.log_results:
+            logging.info("East Second Round Results:")
+            logging.info(east_second_round)
+
+        south_second_round = self.south.second_round(south_first_round)
+        if self.log_results:
+            logging.info("South Second Round Results:")
+            logging.info(south_second_round)
+
+        midwest_second_round = self.midwest.second_round(midwest_first_round)
+        if self.log_results:
+            logging.info("Midwest Second Round Results:")
+            logging.info(midwest_second_round)
+
+        return (
+            west_second_round,
+            east_second_round,
+            south_second_round,
+            midwest_second_round,
+        )
+
+    def sweet_sixteen(
+        self,
+        west_second_round,
+        east_second_round,
+        south_second_round,
+        midwest_second_round,
+    ):
+        west_sweet_sixteen = self.west.sweet_sixteen(west_second_round)
+        if self.log_results:
+            logging.info("West Sweet Sixteen Results:")
+            logging.info(west_sweet_sixteen)
+
+        east_sweet_sixteen = self.east.sweet_sixteen(east_second_round)
+        if self.log_results:
+            logging.info("East Sweet Sixteen Results:")
+            logging.info(east_sweet_sixteen)
+
+        south_sweet_sixteen = self.south.sweet_sixteen(south_second_round)
+        if self.log_results:
+            logging.info("South Sweet Sixteen Results:")
+            logging.info(south_sweet_sixteen)
+
+        midwest_sweet_sixteen = self.midwest.sweet_sixteen(midwest_second_round)
+        if self.log_results:
+            logging.info("Midwest Sweet Sixteen Results:")
+            logging.info(midwest_sweet_sixteen)
+
+        return (
+            west_sweet_sixteen,
+            east_sweet_sixteen,
+            south_sweet_sixteen,
+            midwest_sweet_sixteen,
+        )
+
+    def elite_eight(
+        self,
+        west_sweet_sixteen,
+        east_sweet_sixteen,
+        south_sweet_sixteen,
+        midwest_sweet_sixteen,
+    ):
+        west_winner = self.west.elite_eight(west_sweet_sixteen)
+        if self.log_results:
+            logging.info(f"West Group Winner: {west_winner}")
+
+        east_winner = self.east.elite_eight(east_sweet_sixteen)
+        if self.log_results:
+            logging.info(f"East Group Winner: {east_winner}")
+
+        south_winner = self.south.elite_eight(south_sweet_sixteen)
+        if self.log_results:
+            logging.info(f"South Group Winner: {south_winner}")
+
+        midwest_winner = self.midwest.elite_eight(midwest_sweet_sixteen)
+        if self.log_results:
+            logging.info(f"Midwest Group Winner: {midwest_winner}")
+
+        return west_winner, east_winner, south_winner, midwest_winner
 
 
 class Group:
@@ -68,11 +248,15 @@ class Group:
         if self.log_results:
             logging.info("Group Rankings:")
             logging.info(self.ranking_dict)
+        self.first_four_results = None
+        self.first_round_results = None
+        self.second_round_results = None
+        self.sweet_sixteen_results = None
         self.group_winner = None
 
     @property
     def ranking_dict(self):
-        return{
+        return {
             1: self.one,
             2: self.two,
             3: self.three,
@@ -89,28 +273,17 @@ class Group:
             14: self.fourteen,
             15: self.fifteen,
             16: self.sixteen,
-            "Play In": self.play_in
+            "Play In": self.play_in,
         }
-
-    def run(self):
-        self.first_four()
-        first_round_results = self.first_round()
-        second_round_results = self.second_round(first_round_results)
-        sweet_sixteen_results = self.sweet_sixteen(second_round_results)
-        self.group_winner = self.elite_eight(sweet_sixteen_results)
-        if self.log_results:
-            logging.info(f"Group Winner: {self.group_winner}")
-        return self.group_winner
 
     def first_four(self):
         self.ranking_dict[self.play_in_rank] = self.predict(
             self.play_in, self.ranking_dict[self.play_in_rank]
         )
-        if self.log_results:
-            logging.info("First Four Winner: %s", self.ranking_dict[self.play_in_rank])
+        return self.ranking_dict[self.play_in_rank]
 
     def first_round(self):
-        first_round_results = {
+        return {
             "1_16": self.predict(self.one, self.sixteen),
             "2_15": self.predict(self.two, self.fifteen),
             "3_14": self.predict(self.three, self.fourteen),
@@ -120,32 +293,32 @@ class Group:
             "7_10": self.predict(self.seven, self.ten),
             "8_9": self.predict(self.eight, self.nine),
         }
-        if self.log_results:
-            logging.info("First Round Results:")
-            logging.info(first_round_results)
-        return first_round_results
 
     def second_round(self, first_round_results):
-        second_round_results = {
-            "1_8": self.predict(first_round_results["1_16"], first_round_results["8_9"]),
-            "2_7": self.predict(first_round_results["2_15"], first_round_results["7_10"]),
-            "3_6": self.predict(first_round_results["3_14"], first_round_results["6_11"]),
-            "4_5": self.predict(first_round_results["4_13"], first_round_results["5_12"]),
+        return {
+            "1_8": self.predict(
+                first_round_results["1_16"], first_round_results["8_9"]
+            ),
+            "2_7": self.predict(
+                first_round_results["2_15"], first_round_results["7_10"]
+            ),
+            "3_6": self.predict(
+                first_round_results["3_14"], first_round_results["6_11"]
+            ),
+            "4_5": self.predict(
+                first_round_results["4_13"], first_round_results["5_12"]
+            ),
         }
-        if self.log_results:
-            logging.info("Second Round Results:")
-            logging.info(second_round_results)
-        return second_round_results
 
     def sweet_sixteen(self, second_round_results):
-        sweet_sixteen_results = {
-            "1_4": self.predict(second_round_results["1_8"], second_round_results["4_5"]),
-            "2_3": self.predict(second_round_results["2_7"], second_round_results["3_6"]),
+        return {
+            "1_4": self.predict(
+                second_round_results["1_8"], second_round_results["4_5"]
+            ),
+            "2_3": self.predict(
+                second_round_results["2_7"], second_round_results["3_6"]
+            ),
         }
-        if self.log_results:
-            logging.info("Sweet Sixteen Results")
-            logging.info(sweet_sixteen_results)
-        return sweet_sixteen_results
 
     def elite_eight(self, sweet_sixteen_results):
         return self.predict(sweet_sixteen_results["1_4"], sweet_sixteen_results["2_3"])
