4a034b18c058eb7ad253ac128d1d4305e361186c
Andy Gnias <AGnias47@gmail.com>
2023-06-25 17:46:52 -0400

    Generate 3D plot

--- visualize_weight_functions.py
+++ visualize_weight_functions.py
@@ -4,12 +4,15 @@
 Module for determining weights in probability of team winning
 """
 
+
+import numpy as np
 import matplotlib.pyplot as plt
-from weight.lptr import lptr_tournament_only
+
+from weight.lptr import lptr_tournament_only, lptr_with_ap
 from weight.sigmodal import sigmodal_tournament_only, sigmodal_k_tournament_only
 
 
-def visualize_weight_functions():
+def visualize_2d_weight_functions():
     """
     Allows for visual comparison of how weight functions operate on rankings.
 
@@ -50,5 +53,51 @@ def visualize_weight_functions():
     plt.show()
 
 
+def visualize_3d_weight_functions():
+    """
+    Allows for visual comparison of how weight functions operate on rankings that
+    consider both tournament and AP rankings.
+
+    Resources
+    ---------
+    * https://chat.openai.com/share/33d48396-192d-4237-b55c-60124783a1eb
+
+    Returns
+    -------
+    None
+    """
+    rankings = np.arange(1, 16)
+    ap_rankings = np.arange(1, 26)
+    rankings, ap_rankings = np.meshgrid(rankings, ap_rankings)
+    # Calculate probabilities using lptr function
+    probabilities = np.zeros_like(rankings, dtype=float)
+    for i in range(rankings.shape[0]):
+        for j in range(rankings.shape[1]):
+            res = lptr_with_ap(
+                rankings[i, j],
+                17 - rankings[i, j],
+                ap_rankings[i, j],
+                27 - ap_rankings[i, j],
+            )
+            probabilities[i, j] = res[0]
+    print(probabilities)
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Plot the surface
+    ax.plot_surface(rankings, ap_rankings, probabilities, cmap="viridis")
+
+    # Set labels and title
+    ax.set_xlabel("Tournament Ranking")
+    ax.set_ylabel("AP Ranking")
+    ax.set_zlabel("Probability")
+    ax.set_title("Probability of Team 1 Winning")
+
+    # Show the plot
+    plt.show()
+
+
 if __name__ == "__main__":
-    visualize_weight_functions()
+    visualize_2d_weight_functions()
+    visualize_3d_weight_functions()
