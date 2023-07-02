841dba60797562daf2ac5566f1959dced3eec582
Andy Gnias <AGnias47@gmail.com>
2023-07-02 19:24:01 -0400

    Updated colors; API not ideal

--- data/colors.py
+++ data/colors.py
@@ -3,6 +3,7 @@ Module for gathering and parsing data on school colors
 """
 
 from data.espn import get_teams_from_api, get_name
+from models.colors import Colors
 import webcolors
 
 
@@ -47,7 +48,8 @@ def closest_colour(requested_hex):
 
 def get_color_name(hex_color):
     """
-    Gets a color name from a hex color value
+    Gets a color name from a hex color value, then further simplifies it using
+    COLOR_DICT
 
     Parameters
     ----------
@@ -60,6 +62,77 @@ def get_color_name(hex_color):
     if not hex_color:
         return None
     try:
-        return webcolors.hex_to_name(hex_color)
+        color = webcolors.hex_to_name(hex_color)
     except ValueError:
-        return closest_colour(hex_color)
+        color = closest_colour(hex_color)
+    try:
+        return COLOR_DICT[color]
+    except KeyError:
+        raise ValueError(f"Color {color} not found in COLOR_DICT")
+
+
+COLOR_DICT = {
+    "midnightblue": Colors.NAVY,
+    "darkcyan": Colors.TEAL,
+    "darkolivegreen": Colors.GREEN,
+    "deepskyblue": Colors.LIGHT_BLUE,
+    "mediumturquoise": Colors.LIGHT_BLUE,
+    "darkslateblue": Colors.PURPLE,
+    "darkgoldenrod": Colors.GOLD,
+    "darkslategray": Colors.GRAY,
+    "olive": Colors.GREEN,
+    "darkgray": Colors.GRAY,
+    "saddlebrown": Colors.BROWN,
+    "darkblue": Colors.NAVY,
+    "sienna": Colors.BROWN,
+    "darksalmon": Colors.ORANGE,
+    "cornflowerblue": Colors.LIGHT_BLUE,
+    "limegreen": Colors.GREEN,
+    "tomato": Colors.RED,
+    "firebrick": Colors.RED,
+    "mediumblue": Colors.BLUE,
+    "skyblue": Colors.LIGHT_BLUE,
+    "lavender": Colors.PURPLE,
+    "indigo": Colors.PURPLE,
+    "palegoldenrod": Colors.GOLD,
+    "tan": Colors.BROWN,
+    "lightslategray": Colors.GRAY,
+    "brown": Colors.BROWN,
+    "linen": Colors.WHITE,
+    "white": Colors.WHITE,
+    "peru": Colors.BROWN,
+    "darkkhaki": Colors.BROWN,
+    "sandybrown": Colors.ORANGE,
+    "gold": Colors.GOLD,
+    "red": Colors.RED,
+    "forestgreen": Colors.GREEN,
+    "seagreen": Colors.GREEN,
+    "black": Colors.BLACK,
+    "whitesmoke": Colors.WHITE,
+    "gray": Colors.GRAY,
+    "orangered": Colors.ORANGE,
+    "gainsboro": Colors.GRAY,
+    "chocolate": Colors.BROWN,
+    "dodgerblue": Colors.BLUE,
+    "crimson": Colors.RED,
+    "goldenrod": Colors.GOLD,
+    "cadetblue": Colors.BLUE,
+    "maroon": Colors.RED,
+    "teal": Colors.TEAL,
+    "dimgray": Colors.GRAY,
+    "darkgreen": Colors.GREEN,
+    "purple": Colors.PURPLE,
+    "orange": Colors.ORANGE,
+    "silver": Colors.GRAY,
+    "navy": Colors.NAVY,
+    "darkorange": Colors.ORANGE,
+    "lightblue": Colors.LIGHT_BLUE,
+    "khaki": Colors.BROWN,
+    "wheat": Colors.BROWN,
+    "darkred": Colors.RED,
+    "rosybrown": Colors.BROWN,
+    "coral": Colors.ORANGE,
+    "burlywood": Colors.BROWN,
+    "steelblue": Colors.BLUE,
+    "lightgray": Colors.GRAY,
+}
